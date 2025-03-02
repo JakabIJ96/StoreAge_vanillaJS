ae9c5afed037dbc92a6160c341b9489a8495ac74
JakabIJ96 <jakabij1996@gmail.com>
2025-03-02 18:57:43 +0100

    Modify Add form to be more friendly for the user

--- Code/storage.js
+++ Code/storage.js
@@ -1,16 +1,16 @@
 class Item{
-    constructor(id, name, amount){
-        this.id = id;
+    constructor(name, amount, price){
         this.name = name;
         this.amount = amount;
+        this.price = price;
     }
 };
 
 var storageItems = [
-    new Item(0, "Red polo", 12),
-    new Item(1, "Blue polo", 28),
-    new Item(2, "Green polo", 6),
-    new Item(3, "Yellow polo", 17)
+    new Item("Red polo", 12, 12000),
+    new Item("Blue polo", 28, 28000),
+    new Item("Green polo", 6, 6000),
+    new Item("Yellow polo", 17, 17000)
 ];
 
 const TableRowIdString = "storageTable";
@@ -40,6 +40,7 @@ function loadUpTableWithElements()
         newTableRow.innerHTML = `
             <td>${item.name}</td>
             <td>${item.amount}</td>
+            <td>${item.price}</td>
             <td><button onclick="deleteStorageItem('${newTableRowId}')">Delete</button></td>
             <td><button onclick="editRow('${newTableRowId}')">Edit</button></td>
         `;
@@ -50,55 +51,60 @@ function loadUpTableWithElements()
 }
 
 
+function addNewStoreItemToTable(event) {
+    event.preventDefault(); // Do not reload the page
+
+    let nameValue = document.getElementById("newNameInput").value;
+    let amountValue = document.getElementById("newAmountInput").value;
+    let priceValue = document.getElementById("newPriceInput").value;
+
+
+    if (!nameValue.trim() || !amountValue.trim() || !priceValue.trim()) {
+        alert("Please fill both fields!");
+        return;
+    }
+
+    if(!Number(amountValue) || !Number(priceValue)){
+        alert(`Please use number in ${AmountColumnName} and Price fields!`);
+        return;
+    }
+
+    //DB operation in the future
+    storageItems.push(new Item(nameValue, amountValue, priceValue));
+    //---
+
+    let tableElement = document.getElementById("itemTable");
+    let tableRowId = Number(tableElement.lastChild.id.at(-1)) + 1;
+
+    let newTableRow = document.createElement("tr");
+
+    newTableRow.innerHTML = `
+        <td>${nameValue}</td>
+        <td>${amountValue}</td>
+        <td>${priceValue}</td>
+        <td><button onclick="deleteStorageItem('${TableRowIdString + tableRowId}')">Delete</button></td>
+        <td><button onclick="editRow('${TableRowIdString + tableRowId}')">Edit</button></td>
+    `;
+
+    newTableRow.id = `${TableRowIdString + tableRowId}`;
+
+    tableElement.appendChild(newTableRow);
+
+    let newValuesForm = document.getElementById("newStorageItemForm");
+
+    newValuesForm.setAttribute("style", "visibility: hidden;")
+    document.getElementById("newStorageItemButton").setAttribute("style", "visibility: visible;")
+}
+
 function itemAddForm() {
     console.log("Item add form clicked");
 
-    let newForm = document.createElement("form");
-    newForm.innerHTML = `
-        <label for="newNameInput">Item Name</label>
-        <input type="text" id="newNameInput">
+    document.getElementById("newStorageItemButton").setAttribute("style", "visibility: hidden;")
 
-        <label for="newAmountInput">Item's amount in stock</label>
-        <input type="text" id="newAmountInput">
+    let newForm = document.getElementById("newStorageItemForm");
+    newForm.setAttribute("style", "visibility: visible;")
 
-        <button type="submit">Add items</button>
-    `;
-
-    newForm.addEventListener("submit", function (event) {
-        event.preventDefault(); // Do not reload the page
-
-        let nameValue = document.getElementById("newNameInput").value;
-        let amountValue = document.getElementById("newAmountInput").value;
-
-        if (!nameValue.trim() || !amountValue.trim()) {
-            alert("Please fill both fields!");
-            return;
-        }
-
-        if(!Number(amountValue)){
-            alert(`Please use number in ${AmountColumnName} field!`);
-            return;
-        }
-
-        let tableElement = document.getElementById("itemTable");
-        let tableRowId = Number(tableElement.lastChild.id.at(-1)) + 1;
-
-        let newTableRow = document.createElement("tr");
-
-        newTableRow.innerHTML = `
-            <td>${nameValue}</td>
-            <td>${amountValue}</td>
-            <td><button onclick="deleteStorageItem('${TableRowIdString + tableRowId}')">Delete</button></td>
-            <td><button onclick="editRow('${TableRowIdString + tableRowId}')">Edit</button></td>
-        `;
-
-        newTableRow.id = `${TableRowIdString + tableRowId}`;
-
-        tableElement.appendChild(newTableRow);
-        document.getElementById("body").removeChild(newForm);
-    });
-
-    document.getElementById("body").appendChild(newForm);
+    newForm.addEventListener("submit", addNewStoreItemToTable);
 }
 
 
@@ -113,6 +119,10 @@ function editRow(tableRowId) {
     let tableRow = document.getElementById(tableRowId);
     let tableHeaderNames = document.getElementById("itemTableHeaderRow");
 
+    //DB operation in the future
+    let currentId = Number(tableRowId.at(-1));
+    //---
+
     let count = 0;
     for(let headerName of tableHeaderNames.cells){
         let currentTableCell = tableRow.cells[count];
@@ -123,12 +133,35 @@ function editRow(tableRowId) {
                 alert(`Please use number in ${AmountColumnName} field!`);
                 return;
             }
+
+            //DB operation in the future
+            storageItems[currentId].amount = newValue;
+            //---
+        }
+
+        if(headerName.innerText === "Price"){
+            if(!Number(newValue)){
+                alert(`Please use number in Price field!`);
+                return;
+            }
+
+            //DB operation in the future
+            storageItems[currentId].price = newValue;
+            //---
         }
 
         if (newValue !== null) {
             currentTableCell.innerText = newValue;
+
+
+            //DB operation in the future
+            if(headerName.innerText === "Item name"){
+                storageItems[currentId].name = newValue;
+            }
+            //---
         }
 
         count += 1;
     }
 }
+
